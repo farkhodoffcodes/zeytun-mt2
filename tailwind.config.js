2b3dd697bc0e7dc53c581a7ec1a13180a3c32753
Abdulvohis <abdulvohidcode@gmail.com>
2024-06-14 11:36:31 +0300

    Flowbite done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
+    "./src/**/*.{html,tsx,js,ts,jsx}",
     'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
