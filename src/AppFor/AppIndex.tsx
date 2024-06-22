2883949b09d1d0717dd9f24c463577ae6cd58f2c
user.email <hayotabdurahimov@gmail.com>
2024-06-22 13:18:31 +0500

    rename

--- src/AppFor/AppIndex.tsx
+++ src/AppFor/AppIndex.tsx
@@ -1,7 +1,7 @@
 import Footer from "../Components/Footer"
 import Header from "../Components/Header"
 import Hero from "../Components/Hero"
-import Product from "../Components/Product"
+import Product from "../Components/Products"
 
 const AppIndex = () => {
   return (
