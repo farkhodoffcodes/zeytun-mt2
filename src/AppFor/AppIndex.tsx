335a48954a70bc3a656c3a0ee1ee3067edbdc5a4
Umarova Si <133396995+UmarovaSi@users.noreply.github.com>
2024-06-23 20:39:40 +0500

    fixed mistakes

--- src/AppFor/AppIndex.tsx
+++ src/AppFor/AppIndex.tsx
@@ -1,18 +1,14 @@
 import { Link } from "react-router-dom"
 import Footer from "../Components/Footer"
 import Header from "../Components/Header"
-<<<<<<< HEAD
-import Product from "../Components/Product"
-import Category from "../Components/Category"
-=======
 import Hero from "../Components/Hero"
 import Product from "../Components/Products"
->>>>>>> 2883949b09d1d0717dd9f24c463577ae6cd58f2c
+import Category from "../Components/Category"
 
 const AppIndex = () => {
   return (
     <div>
-      <Header />
+      <Header/>
       <Hero/>
       <Product/>
       <Footer/>
